f1a3ec9abd54e4a97813d26783c7c892f8c04643
AnselTeng629 <87511068+AnselTeng629@users.noreply.github.com>
2026-01-20 15:18:07 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "pace-speed-ios-v1";
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./apple-touch-icon.png"
+];
+
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(FILES))
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
